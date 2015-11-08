d3fefe2fe068d001f192ffbb555c048e9cf22822
Ascharapon Sinlamuth <greatlife224@gmail.com>
2015-11-08 17:30:00 +0700

    code Player, City create King

--- 2110215_2015_FinalProject/src/logic/City.java
+++ 2110215_2015_FinalProject/src/logic/City.java
@@ -1,5 +1,13 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class City {
+	
+	protected int x, y;
+	protected Player owner;
+	protected ArrayList<Soldier> soldiersInCity;
+	
+	
 
 }
--- 2110215_2015_FinalProject/src/logic/Player.java
+++ 2110215_2015_FinalProject/src/logic/Player.java
@@ -1,5 +1,27 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class Player {
+	
+	protected ArrayList<City> myCities;
+	protected ArrayList<Soldier> mySoldiers;
+	protected boolean isLose;
+	protected String name;
+	protected int completedObjective;
+	protected int food, gold;
+	
+	public Player(String name, int food, int gold) {
+		this.name = name;
+		isLose = false;
+		completedObjective = 0;
+		myCities = new ArrayList<City>();
+		mySoldiers = new ArrayList<Soldier>();
+		this.food = food;
+		this.gold = gold;
+		mySoldiers.add(new King());
+	}
+	
+	
 
 }
